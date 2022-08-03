e52dbbc8bd58a418bc881a025e2bf1013ea9392e
LemeshN <lemesh.mikalai@gmail.com>
2022-08-03 12:58:45 +0300

    Django
    Signed-off-by: LemeshN <lemesh.mikalai@gmail.com>

--- vebsite10/main/views.py
+++ vebsite10/main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
 
-def index(request):
-    tasks = Task.objects.all()
-    return render(request, 'main.html', {'title':'Главная страница'}, {'tasks':tasks})
+def main(request):
+    tasks = Task.objects.order_by('id')
+    return render(request, 'main.html', {'title':'Главная страница', 'tasks':'title'})
 
 def Hello(request):
     return HttpResponse("<h3>Приветик :)</h3>")
